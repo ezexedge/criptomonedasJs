6a6006afa61e9308cc5424d8a98168b173595ba8
ezequiel <ezeedge@gmail.com>
2019-04-10 17:12:52 -0300

    subiendo-todo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import imagen from './cryptomonedas.png'
+import Formulario from './componentes/Formulario'
+
+class App extends Component {
+  render() {
+    return (
+   <div className="container">
+    <div className="row">
+      <div className="one-half column">
+        <img src={imagen} alt="imagen" className="logotipo" />
+      </div>
+      <div className="one-half column">
+            <h1>Cotiza criptomonedas al instante</h1>
+              <Formulario/>
+      </div>
+    </div>
+   </div>
+    );
+  }
+}
+
+export default App;
